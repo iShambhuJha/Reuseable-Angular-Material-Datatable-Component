7bfcbf7412a94d90a2e2dde17f453a8df54378a4
Shambhu Jha <shambhu.jha014@gmail.com>
2020-04-23 09:07:13 +0530

    created required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialDatatableComponent } from './shared/material-datatable/material-datatable.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+import { FacultyDetailsComponent } from './faculty-details/faculty-details.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterialDatatableComponent,
+    StudentDetailsComponent,
+    FacultyDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/material-datatable/material-datatable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-material-datatable',
+  templateUrl: './material-datatable.component.html',
+  styleUrls: ['./material-datatable.component.css']
+})
+export class MaterialDatatableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
